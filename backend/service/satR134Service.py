25618d5f6621a2bc86e7b6d0b6d997c48b5cfa3d
Vlad Ciubotaru <ciubo001@gmail.com>
2025-07-03 15:12:37 -0400

    finished r134 pressure/temp

--- backend/service/satR134Service.py
+++ backend/service/satR134Service.py
@@ -1,13 +1,14 @@
 from backend.core.calculations.satR134T import Sat134aTCalculation
+from backend.core.calculations.satR134P import Sat134aPCalculation
 
 def sat_r134(fluid_type: str, inputs: dict):
     print("fluid_type: ", fluid_type)
     print("inputs: ", inputs)
-    if fluid_type == "satR134T":
+    if fluid_type in ["satR134T"]:
         sat134 = Sat134aTCalculation(inputs=inputs)
         result = sat134.calculate()
         return result
-    # else:
-    #     satWaterT = SatWaterTCalculation(inputs=inputs)
-    #     result = satWaterT.calculate()
-    #     return result
+    else:
+        sat134 = Sat134aPCalculation(inputs=inputs)
+        result = sat134.calculate()
+        return result
